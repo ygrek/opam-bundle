d8fa3d4445f46397283efa02b32431552315989a
ygrek <ygrek@autistici.org>
2015-08-08 15:24:20 -0700

    upgrade for opam-lib master, compiler bundling disabled

--- src/main.ml
+++ src/main.ml
@@ -12,21 +12,22 @@ let resolve_deps t atoms =
   let atoms = OpamSolution.sanitize_atom_list t ~permissive:true atoms in
   let action = Install (OpamPackage.Name.Set.of_list (List.map fst atoms)) in
   let universe = { (OpamState.universe t action) with u_installed = OpamPackage.Set.empty; } in
-  let request = { wish_install = atoms; wish_remove = []; wish_upgrade = []; criteria = `Default; } in
-  let requested = OpamPackage.Name.Set.of_list @@ List.map fst atoms in
-  match OpamSolver.resolve ~verbose:true universe ~requested ~orphans:OpamPackage.Set.empty request with
+  let request = { wish_install = atoms; wish_remove = []; wish_upgrade = []; criteria = `Default; extra_attributes = []; } in
+  match OpamSolver.resolve ~verbose:true universe ~orphans:OpamPackage.Set.empty request with
   | Success solution ->
-    (* return in order *)
+    (* OpamSolver.new_packages, but return in order *)
     let l = OpamSolver.ActionGraph.Topological.fold (fun act acc -> match act with
-        | To_change (_, p) -> p::acc
-        | _ -> acc)
-      solution.to_process []
+        | `Install p | `Change (_,_,p) -> p :: acc
+        | `Reinstall _ | `Remove _ | `Build _  -> acc)
+      (OpamSolver.get_atomic_action_graph solution) []
     in
     List.rev l
   | Conflicts cs ->
-    OpamGlobals.error_and_exit "Conflicts: %s"
+    OpamConsole.error_and_exit "Conflicts: %s"
       (OpamCudf.string_of_conflict (OpamState.unavailable_reason t) cs)
 
+(** see OpamState.install_compiler *)
+(*
 let bundle_compiler ~reuse ~dryrun (t:OpamState.state) archive =
   let open OpamFilename in
   let repo_name =
@@ -35,23 +36,23 @@ let bundle_compiler ~reuse ~dryrun (t:OpamState.state) archive =
       "unknown repo"
       (OpamState.repository_and_prefix_of_compiler t t.compiler)
   in
-  OpamGlobals.msg "Bundling compiler %s [%s]\n" (OpamCompiler.to_string t.compiler) repo_name;
+  OpamConsole.msg "Bundling compiler %s [%s]\n" (OpamCompiler.to_string t.compiler) repo_name;
   let comp_f = OpamPath.compiler_comp t.root t.compiler in
   match exists comp_f with
-  | false -> OpamGlobals.error_and_exit "Cannot bundle compiler %s: no such file : %s"
+  | false -> OpamConsole.error_and_exit "Cannot bundle compiler %s: no such file : %s"
       (OpamCompiler.to_string t.compiler)
       (to_string comp_f);
   | true ->
   let comp = OpamFile.Comp.read comp_f in
   match OpamFile.Comp.preinstalled comp with
-  | true -> OpamGlobals.error_and_exit "Cannot bundle preinstalled compiler %s" (OpamCompiler.to_string t.compiler)
+  | true -> OpamConsole.error_and_exit "Cannot bundle preinstalled compiler %s" (OpamCompiler.to_string t.compiler)
   | false ->
   match OpamFile.Comp.configure comp @ OpamFile.Comp.make comp <> [] with
   | true -> assert false (* old style comp file - not supported *)
   | false ->
   (* Install the compiler *)
   match OpamFile.Comp.src comp with
-  | None -> OpamGlobals.error_and_exit "No source for compiler %s" (OpamCompiler.to_string t.compiler)
+  | None -> OpamConsole.error_and_exit "No source for compiler %s" (OpamCompiler.to_string t.compiler)
   | Some comp_src ->
   match dryrun with
   | true -> None
@@ -59,23 +60,24 @@ let bundle_compiler ~reuse ~dryrun (t:OpamState.state) archive =
   let build_dir = OpamPath.Switch.build_ocaml t.root t.switch in
   let return = Some (basename_dir build_dir, comp) in
   match reuse && exists archive with
-  | true -> OpamGlobals.msg "Reusing %s\n" (to_string archive); return
+  | true -> OpamConsole.msg "Reusing %s\n" (to_string archive); return
   | false ->
+  let open OpamProcess.Job.Op in
   let kind = OpamFile.Comp.kind comp in
   if kind = `local
   && Sys.file_exists (fst comp_src)
   && Sys.is_directory (fst comp_src) then
     OpamFilename.link_dir
       ~src:(OpamFilename.Dir.of_string (fst comp_src)) ~dst:build_dir
-  else OpamFilename.with_tmp_dir (fun download_dir ->
-      let result =
-        OpamRepository.pull_url kind (OpamPackage.of_string "compiler.get")
-          download_dir None [comp_src] in
-      match result with
-      | Not_available u -> OpamGlobals.error_and_exit "%s is not available." u
+  else
+    OpamProcess.Job.run @@
+    OpamFilename.with_tmp_dir_job begin fun download_dir ->
+      OpamRepository.pull_url kind (OpamPackage.of_string "compiler.get") download_dir None [comp_src]
+      @@| function
+      | Not_available u -> OpamConsole.error_and_exit "%s is not available." u
       | Up_to_date r
       | Result r        -> OpamFilename.extract_generic_file r build_dir
-    );
+  end;
   let patches = OpamFile.Comp.patches comp in
   let patches = List.map (fun f ->
       OpamFilename.download ~overwrite:true f build_dir
@@ -85,20 +87,31 @@ let bundle_compiler ~reuse ~dryrun (t:OpamState.state) archive =
     [ "tar"; "czf"; (to_string archive); Base.to_string (basename_dir build_dir) ]
   ]);
   return
+*)
 
 let bundle ~reuse ~dryrun ~deps_only ~with_compiler bundle atoms =
   let open OpamFilename in
   (* OpamSystem.real_path is pure evil *)
   let bundle =
-    if Filename.is_relative @@ OpamFilename.Dir.to_string bundle then
-      OP.(OpamFilename.cwd () / OpamFilename.Dir.to_string bundle)
+    let open OpamFilename in
+    if Filename.is_relative @@ Dir.to_string bundle then
+      Op.(cwd () / Dir.to_string bundle)
     else
       bundle
   in
-  let archives = OP.(bundle / "archives") in
+  let archives = OpamFilename.Op.(bundle / "archives") in
   if not dryrun && not reuse && exists_dir bundle then
-    OpamGlobals.error_and_exit "Directory %s already exists" (Dir.to_string bundle);
-  let t = OpamState.load_state "bundle" in
+    OpamConsole.error_and_exit "Directory %s already exists" (Dir.to_string bundle);
+  let root = OpamStateConfig.opamroot () in
+  OpamFormatConfig.init ();
+  match OpamStateConfig.load_defaults root with
+  | false -> OpamConsole.error_and_exit "Failed init"
+  | true ->
+  OpamStateConfig.init ~root_dir:root ();
+  OpamRepositoryConfig.init ();
+  OpamSolverConfig.init ();
+  OpamStd.Config.init ();
+  let t = OpamState.load_state "bundle" OpamStateConfig.(!r.current_switch) in
   let atoms = OpamSolution.sanitize_atom_list ~permissive:true t atoms in
   let packages = resolve_deps t atoms in
   let packages = match deps_only with
@@ -115,7 +128,7 @@ let bundle ~reuse ~dryrun ~deps_only ~with_compiler bundle atoms =
       of_string "preinstalled", B (not with_compiler);
       of_string "make", S "$MAKE";
     ]) in
-    OpamVariable.Map.of_list (vars1 @ vars2)
+    OpamVariable.Map.of_list @@ List.map (fun (k,v) -> k, Some v) (vars1 @ vars2)
   in
   let b = Buffer.create 10 in
   (* expecting quoted commands *)
@@ -148,6 +161,8 @@ let bundle ~reuse ~dryrun ~deps_only ~with_compiler bundle atoms =
     List.iter mkdir [bundle; archives];
   if with_compiler then
   begin
+    OpamConsole.msg "compiler bundling - not implemented";
+(*
     let archive = "ocaml." ^ (OpamCompiler.to_string t.compiler) ^ ".tar.gz" in
     let archive_path = OP.(archives // archive) in
     match bundle_compiler ~reuse ~dryrun t archive_path with
@@ -157,6 +172,7 @@ let bundle ~reuse ~dryrun ~deps_only ~with_compiler bundle atoms =
       let commands = OpamState.filter_commands t variables (OpamFile.Comp.build comp) in
       let commands = List.map (fun l -> l @ [">>build.log 2>>build.log || (echo FAILED; tail build.log; exit 1)"]) commands in
       shellout_build ~dir:(Base.to_string dir) ~archive ~env commands
+*)
   end;
   List.iter begin fun nv ->
     try
@@ -166,25 +182,27 @@ let bundle ~reuse ~dryrun ~deps_only ~with_compiler bundle atoms =
           "unknown repo"
           (OpamState.repository_of_package t nv)
       in
-      OpamGlobals.msg "Bundling %s [%s]\n" (OpamPackage.to_string nv) repo_name;
+      OpamConsole.msg "Bundling %s [%s]\n" (OpamPackage.to_string nv) repo_name;
       match dryrun with
       | true -> ()
       | false ->
-      let archive = OP.(archives // (OpamPackage.to_string nv ^ ".tar.gz")) in
+      let archive = OpamFilename.Op.(archives // (OpamPackage.to_string nv ^ ".tar.gz")) in
       if reuse && exists archive then
       begin
-        OpamGlobals.msg "Reusing %s\n" (to_string archive)
+        OpamConsole.msg "Reusing %s\n" (to_string archive)
       end
       else
       begin
         (* gets the source (from url, local path, git, etc) and applies patches and substitutions *)
-        OpamAction.download_package t nv;
-        OpamAction.extract_package t nv;
+        match OpamProcess.Job.run (OpamAction.download_package t nv) with
+        | `Error s -> OpamConsole.error_and_exit "Download failed : %s" s
+        | `Successful s ->
+        OpamAction.extract_package t s nv;
         let p_build = OpamPath.Switch.build t.root t.switch nv in
   (*
-          OpamGlobals.msg "p_build: %s\n" (OpamFilename.Dir.to_string p_build);
-          OpamGlobals.msg "archives: %s\n" (OpamFilename.Dir.to_string archives);
-          OpamGlobals.msg "archive: %s\n" (OpamFilename.to_string archive);
+          OpamConsole.msg "p_build: %s\n" (OpamFilename.Dir.to_string p_build);
+          OpamConsole.msg "archives: %s\n" (OpamFilename.Dir.to_string archives);
+          OpamConsole.msg "archive: %s\n" (OpamFilename.to_string archive);
   *)
         exec (dirname_dir p_build) [
           [ "tar"; "czf"; to_string archive; Base.to_string (basename_dir p_build) ]
@@ -192,29 +210,69 @@ let bundle ~reuse ~dryrun ~deps_only ~with_compiler bundle atoms =
       end;
       let archive = Base.to_string (basename archive) in
       let opam = OpamState.opam t nv in (* dev? *)
-      let env = OpamState.add_to_env t ~opam [] (OpamFile.OPAM.build_env opam) variables in
+      let env = OpamState.add_to_env t ~opam [] (OpamFile.OPAM.build_env opam) ~variables in
       let install = OpamFile.Dot_install.safe_read (OpamPath.Switch.build_install t.root t.switch nv) in
-      let commands = OpamState.filter_commands t ~opam variables (OpamFile.OPAM.build opam) in
+      let commands = OpamFile.OPAM.build opam @ OpamFile.OPAM.install opam in
+      let commands = OpamFilter.commands (OpamState.filter_env t ~opam ~local_variables:variables) commands in
       let commands = List.map (fun l -> l @ [">>build.log 2>>build.log || (echo FAILED; tail build.log; exit 1)"]) commands in
       let install_commands = ref [] in
-      OpamAction.perform_dot_install (OpamPackage.name nv) install
-          (`Shell (fun l -> install_commands := l :: !install_commands))
-          (OpamSwitch.of_string "$BUNDLE_PREFIX");
+      let switch = OpamSwitch.of_string "$BUNDLE_PREFIX" in
+      OpamAction.perform_dot_install { t with switch } (OpamPackage.name nv) install
+          (`Shell (fun l -> install_commands := l :: !install_commands));
       shellout_build ~archive ~env (commands @ List.rev !install_commands);
     with e ->
-      OpamMisc.fatal e;
-      OpamGlobals.error_and_exit "%s" (Printexc.to_string e);
+      OpamStd.Exn.fatal e;
+      OpamConsole.error_and_exit "%s" (Printexc.to_string e);
   end packages;
   match dryrun with
   | true -> ()
   | false ->
-  let install_sh = OP.(bundle // "install.sh") in
+  let install_sh = OpamFilename.Op.(bundle // "install.sh") in
   write install_sh (Buffer.contents b);
   chmod install_sh 0o755;
   ()
 
 let cmd =
   let open Cmdliner in
+  (* / various bits from OpamArg *)
+  let dirname =
+    let parse str = `Ok (OpamFilename.Dir.of_string str) in
+    let print ppf dir = Format.pp_print_string ppf (OpamFilename.prettify_dir dir) in
+    parse, print
+  in
+  let nonempty_arg_list name doc conv =
+    let doc = Arg.info ~docv:name ~doc [] in
+    Arg.(non_empty & pos_all conv [] & doc)
+  in
+  (* name * version constraint *)
+  let atom =
+    let parse str =
+      let re = Re_str.regexp "\\([^>=<.!]+\\)\\(>=?\\|<=?\\|=\\|\\.\\|!=\\)\\(.*\\)" in
+      try
+        if not (Re_str.string_match re str 0) then failwith "no_version";
+        let sname = Re_str.matched_group 1 str in
+        let sop = Re_str.matched_group 2 str in
+        let sversion = Re_str.matched_group 3 str in
+        let name = OpamPackage.Name.of_string sname in
+        let sop = if sop = "." then "=" else sop in
+        let op = OpamFormula.relop_of_string sop in (* may raise Invalid_argument *)
+        let version = OpamPackage.Version.of_string sversion in
+        `Ok (name, Some (op, version))
+      with Failure _ | Invalid_argument _ ->
+        try `Ok (OpamPackage.Name.of_string str, None)
+        with Failure msg -> `Error msg
+    in
+    let print ppf atom =
+      Format.pp_print_string ppf (OpamFormula.short_string_of_atom atom) in
+    parse, print
+  in
+  let nonempty_atom_list =
+    nonempty_arg_list "PACKAGES"
+      "List of package names, with an optional version or constraint, \
+       e.g `pkg', `pkg.1.0' or `pkg>=0.5'."
+      atom
+  in
+  (* / *)
   let doc = "Generate a self-contained bundle of given packages with all dependencies." in
   let man = [
     `S "DESCRIPTION";
@@ -223,9 +281,9 @@ let cmd =
         along with the shell script to unpack, build and install those packages \
         on any remote machine (even without OPAM installed).";
   ] in
-  let outdir = 
+  let outdir =
     let doc = Arg.info ["o";"outdir"] ~docv:"DIR" ~doc:"Write bundle to the directory $(docv)." in
-    Arg.(value & opt OpamArg.dirname (OpamFilename.Dir.of_string "bundle") & doc)
+    Arg.(value & opt dirname (OpamFilename.Dir.of_string "bundle") & doc)
   in
   let deps_only =
     Arg.(value & flag & info ["deps-only"] ~doc:"Bundle only the dependencies, excluding the specified packages.")
@@ -242,11 +300,10 @@ let cmd =
     in
     Arg.(value & flag & info ["reuse"] ~doc)
   in
-  let bundle global_options deps_only dryrun with_compiler reuse outdir names =
-    OpamArg.apply_global_options global_options;
+  let bundle deps_only dryrun with_compiler reuse outdir names =
     bundle ~dryrun ~deps_only ~with_compiler ~reuse outdir names
   in
-  Term.(pure bundle $OpamArg.global_options $deps_only $dryrun $with_compiler $reuse $outdir $OpamArg.nonempty_atom_list),
+  Term.(pure bundle $deps_only $dryrun $with_compiler $reuse $outdir $nonempty_atom_list),
   Term.info "opam-bundle" ~doc ~man
 
 let () = match Cmdliner.Term.eval cmd with `Error _ -> exit 1 | _ -> exit 0
